f9ddb88b48ddd027a0d0dccaa07c8361599ea3eb
rouzbehtondkar <tondkar.eng@gmail.com>
2024-04-23 23:09:09 +0330

    index detail

---
+++ pages/ِِDetail/index.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <div class="w-[100%] h-[20vh] bg-black text-white">
+    <div class="w-[100%] flex justify-end pl-[165px] pt-[7px]">
+      <button class="bg-red">LOGO</button>
+    </div>
+    <div class="w-[100%] flex justify-center items-center">
+
+
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
