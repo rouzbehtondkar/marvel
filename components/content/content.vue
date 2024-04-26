858ce7df9420efd25feb87fb1b53cda0448b2fbf
rouzbehtondkar <tondkar.eng@gmail.com>
2024-04-26 14:46:54 +0330

    update

--- components/content/content.vue
+++ components/content/content.vue
@@ -1,6 +1,6 @@
 <template>
   <!--  search and header-->
-  <div class="w-[100%] h-[20vh] bg-black text-white">
+  <div class="w-[100%] h-[20vh] text-white gradient-circle-red">
     <div class="w-[100%] flex justify-end pl-[165px] pt-[7px]">
       <button class="bg-red">LOGO</button>
     </div>
@@ -10,30 +10,36 @@
              placeholder="Searching for characters">
     </div>
   </div>
-  <!--  content-->
+  <!--  content card Characters -->
   <div class="bg-gray-800 h-[80vh]">
     <div class="w-full bg-gray-800 flex flex-col items-center justify-center p-4">
 
 <!--      show data from backend-->
       <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4" dir="ltr">
         <div v-for="(item, i) in displayedCharacters" :key="i" class="bg-gray-700 text-white p-4 rounded-lg shadow-lg">
-          <img :src="item.thumbnail.path + '.' + item.thumbnail.extension" alt="Character Thumbnail"
-               class="w-full h-[269px] object-cover rounded-lg">
+<!--  diirect to other page for show detail       -->
+          <NuxtLink :to="`/detail/${item.id}`" >
+            <img :src="item.thumbnail.path + '.' + item.thumbnail.extension" alt="Character Thumbnail"
+                 class="w-full h-[269px] object-cover rounded-lg">
+          </NuxtLink>
+
           <NuxtLink :to="`/detail/${item.id}`"  class="text-center">{{ item.name }}</NuxtLink>
         </div>
       </div>
     </div>
 <!--    section about pagenation-->
-    <div class=" bg-gray-800 flex justify-center mt-4 pb-[40px]">
+    <div class=" bg-gray-800 flex justify-center items-center  mt-4 pb-[40px]">
+    <span class="text-white">  <</span>
       <button
           v-for="page in totalPages"
           :key="page"
           @click="changePage(page)"
-          class="mx-1 px-3 py-2 border border-gray-300 text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
-          :class="{ 'bg-blue-500 text-white': currentPage === page }"
+          class="mx-1 px-3 py-2  border-gray-300 text-sm leading-4 font-medium rounded-md text-white  hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
+          :class="{ 'bg-red-500 text-white': currentPage === page }"
       >
         {{ page }}
       </button>
+      <span class="text-white">  ></span>
     </div>
   </div>
 
@@ -44,7 +50,7 @@
 <script setup lang="ts">
 //import files from vue and component
 import {useMarvelStore} from '~/stores/useMarvelStore ';
-import {ref} from 'vue';
+import {ref,computed,watchEffect } from 'vue';
 
 // variabel
 const specificCharacter = ref(null);
@@ -52,6 +58,7 @@ const searchTerm = ref('');
 const marvelStore = useMarvelStore();
 const currentPage = ref(1);
 const itemsPerPage = 6;
+const searchClicked = ref(false);
 
 
 //run data from backend
@@ -59,45 +66,49 @@ onMounted(() => {
   marvelStore.fetchCharacters();
 
 });
-// search items marvel
-const searchCharacters = async () => {
-  try {
-    await marvelStore.fetchCharacters(searchTerm.value);
-    searchTerm.value = '';
 
-  } catch (error) {
-    console.error('Error searching characters:', error);
-  }
-};
+// const searchCharacters = async () => {
+//   try {
+//     await marvelStore.fetchCharacters(searchTerm.value);
+//     searchTerm.value = '';
+//
+//   } catch (error) {
+//     console.error('Error searching characters:', error);
+//   }
+// };
 
-
+// funcation filter for search
 const filteredCharacters = computed(() => {
+  if (!searchClicked) return [] ;
   return marvelStore.characters.filter(character => character.name.toLowerCase().includes(searchTerm.value.toLowerCase()));
 });
 
 
-
-
-
+// funcation pagenation for number page
 const totalPages = computed(() => {
   return Math.ceil(filteredCharacters.value.length / itemsPerPage);
 });
+
 
 const displayedCharacters = computed(() => {
   const start = (currentPage.value - 1) * itemsPerPage;
   const end = start + itemsPerPage;
   return filteredCharacters.value.slice(start, end);
-});
 
+});
+// run funcation pagenation
 const changePage = (page) => {
   currentPage.value = page;
 };
+console.log(currentPage.value)
 
-// const loadSpecificCharacter = async (characterId) => {
-//   specificCharacter.value = await marvelStore.fetchCharacterById(characterId);
-// };
 </script>
 
 <style scoped>
-
+//gradiant for header
+.gradient-circle-red {
+  background-image: linear-gradient(to right, rgb(136, 0, 0) 1%, black 10%);
+  background-position: left center;
+  //background-size: 100% 100%;
+}
 </style>
--- pages/detail/[id].vue
+++ pages/detail/[id].vue
@@ -1,24 +1,27 @@
 <template>
-  <div class="w-[100%] h-[120vh] bg-black text-white">
-
+<!--  header info for Characters-->
+  <div class="w-[100%] h-[120vh] bg-black text-white ">
     <button class="bg-red md:mr-[88%] mt-[20px] logo_res">LOGO</button>
     <div v-for="(item,i) in detailInfo" :key="i">
       <div class="flex w-[100%] pl-[10px] gap-[20px] pb-[40px] items-center detailInfo_res ">
         <div class="w-[80%] flex flex-col md:pt-[80px]" dir="ltr">
           <span class="font-bold text-[30px]">{{ item.name }}</span>
           <span>{{ item.description }}</span>
-        <div class=" flex section_btn  justify-start  gap-5 pt-[10px]">
-          <button class="btn-custom flex justify-between items-center gap-2">
-            <img src="~/assets/img/export.png" alt="Comic Link" />
-            Details</button>
-          <button class="btn-custom  flex  justify-center gap-5 ">
-            <img src="~/assets/img/export.png" alt="Comic Link" />
-            wiki</button>
+          <div class=" flex section_btn  justify-start  gap-5 pt-[10px]">
+            <button class="btn-custom flex justify-center items-center gap-2">
+              <img src="~/assets/img/export.png" alt="Comic Link"/>
+              Details
+            </button>
+            <button class="btn-custom  flex  justify-center gap-5 ">
+              <img src="~/assets/img/export.png" alt="Comic Link"/>
+              wiki
+            </button>
+            <button class="btn-custom  flex  justify-center gap-5 ">
+              <img src="~/assets/img/export.png" alt="Comic Link"/>
+              comiclink
+            </button>
 
-          <button class="btn-custom  flex  justify-center gap-5 ">
-            <img src="~/assets/img/export.png" alt="Comic Link" />comiclink</button>
-
-        </div>
+          </div>
         </div>
 
         <div class="md:w-[20%] flex img_section">
@@ -28,11 +31,11 @@
 
       </div>
     </div>
-    <!--    section info-->
+    <!--    section info for comics and series-->
     <div class="bg-gray-800 h-[80vh]">
       <div class="w-full bg-gray-800 flex flex-col items-center justify-center p-4">
 
-        <div class="flex flex-col gap-[50px]" >
+        <div class="flex flex-col gap-[50px]">
           <h3 class="font-bold text-[20px] pb-[50px] pt-[50px] pr-[92%]" dir="ltr"> comics</h3>
           <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4" dir="ltr">
 
@@ -44,11 +47,11 @@
             <!--  section series        -->
 
 
-
           </div>
           <h3 class="font-bold text-[20px] pb-[50px] pt-[50px] pr-[92%]" dir="ltr"> series</h3>
           <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4" dir="ltr">
-            <div v-for="(items,i) in detailseries" :key="i" class=" w-[300px] bg-gray-700 text-white p-4 rounded-lg shadow-lg pt-[50px]">
+            <div v-for="(items,i) in detailseries" :key="i"
+                 class=" w-[300px] bg-gray-700 text-white p-4 rounded-lg shadow-lg pt-[50px]">
               <img :src="items.thumbnail.path + '.' + items.thumbnail.extension" alt="Character Thumbnail"
                    class="w-full h-[269px] object-cover rounded-lg">
               <NuxtLink class="text-center">{{ items.title }}</NuxtLink>
@@ -118,6 +121,7 @@ const marvelsComic = async (characterId) => {
   const publicKey = '2dadec700ec19acae6022f02802c1593';
   const privateKey = 'e9970b149457a34b075505ddb0818f29e1a9430b';
   const apiBaseURL = "https://gateway.marvel.com/v1/public/";
+
   function createURL() {
     const ts = Date.now();
     const params = new URLSearchParams({
